d3178131842661be7207f5a45d5a983380d8f3f2
Maxwell Morris <48025321+m4xmorris@users.noreply.github.com>
2023-01-04 21:07:09 +0000

    Update dns.tf

--- dns.tf
+++ dns.tf
@@ -1,50 +1,61 @@
-resource "cloudflare_record" "verification" {
-  zone_id    = var.cloudflare_zone_id
-  name       = var.domain
-  value      = var.verification_record
-  type       = "TXT"
+locals {
+    mx_records = {
+        "10" = "mail.protonmail.ch"
+        "20" = "mailsec.protonmail.ch"
+    }
+    dkim_records = {
+        "protonmail._domainkey" = "${var.dkim_1}"
+        "protonmail2._domainkey" = "${var.dkim_2}"
+        "protonmail3._domainkey" = "${var.dkim_3}"
+    }
+    txt_records = {
+        "verification" = "${var.verification_record}"
+        "spf" = "${var.spf_record}"
+    }
 }
-resource "cloudflare_record" "mx_1" {
-  zone_id    = var.cloudflare_zone_id
-  name       = var.domain
-  value      = "mail.protonmail.ch"
-  type       = "MX"
-  priority   = "10"
+
+module "mx" {
+    source  = "m4xmorris/dns-record/cloudflare"
+    version = "1.1.0"
+    for_each = {
+        for k, v in local.mx_records : k => v
+    }
+    cloudflare_zone_id = var.cloudflare_zone_id
+    name = var.domain
+    type = "MX"
+    priority = each.key
+    value = each.value
 }
-resource "cloudflare_record" "mx_2" {
-  zone_id    = var.cloudflare_zone_id
-  name       = var.domain
-  value      = "mailsec.protonmail.ch"
-  type       = "MX"
-  priority   = "20"
+
+module "dkim" {
+    source  = "m4xmorris/dns-record/cloudflare"
+    version = "1.1.0"
+    for_each = {
+        for k, v in local.dkim_records : k => v
+    }
+    cloudflare_zone_id = var.cloudflare_zone_id
+    name = "${each.key}.${var.domain}"
+    type = "CNAME"
+    value = each.value
 }
-resource "cloudflare_record" "spf" {
-  zone_id    = var.cloudflare_zone_id
-  name       = var.domain
-  value      = var.spf_record
-  type       = "TXT"
+  
+module "txt" {
+    source  = "m4xmorris/dns-record/cloudflare"
+    version = "1.1.0"
+    for_each = {
+        for k, v in local.txt_records : k => v
+    }
+    cloudflare_zone_id = var.cloudflare_zone_id
+    name = "${var.domain}"
+    type = "TXT"
+    value = each.value
 }
-resource "cloudflare_record" "dkim_1" {
-  zone_id    = var.cloudflare_zone_id
-  name       = "protonmail._domainkey.${var.domain}"
-  value      = var.dkim_1
-  type       = "CNAME"
-}
-resource "cloudflare_record" "dkim_2" {
-  zone_id    = var.cloudflare_zone_id
-  name       = "protonmail2._domainkey.${var.domain}"
-  value      = var.dkim_2
-  type       = "CNAME"
-}
-resource "cloudflare_record" "dkim_3" {
-  zone_id    = var.cloudflare_zone_id
-  name       = "protonmail3._domainkey.${var.domain}"
-  value      = var.dkim_3
-  type       = "CNAME"
-}
-resource "cloudflare_record" "dmarc" {
-  zone_id    = var.cloudflare_zone_id
-  name       = "_dmarc.${var.domain}"
-  value      = "v=DMARC1; p=${var.dmarc_policy}"
-  type       = "TXT"
+  
+module "dmarc" {
+    source  = "m4xmorris/dns-record/cloudflare"
+    version = "1.1.0"
+    cloudflare_zone_id = var.cloudflare_zone_id
+    name = "_dmarc.${var.domain}"
+    type = "TXT"
+    value = "v=DMARC1; p=${var.dmarc_policy}"
 }
