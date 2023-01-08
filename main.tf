82fe9278a2570521879c780f7fe8e355e6ededc8
Maxwell Morris <48025321+maxmorrisio@users.noreply.github.com>
2023-01-08 12:00:49 +0000

    `terraform fmt` and fixed LICENSE.

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "cloudflare_record" "verification" {
-  zone_id    = var.cloudflare_zone_id
-  name       = var.domain
-  value      = var.verification_record
-  type       = "TXT"
+  zone_id = var.cloudflare_zone_id
+  name    = var.domain
+  value   = var.verification_record
+  type    = "TXT"
 }
 
 resource "cloudflare_record" "mx" {
@@ -10,35 +10,35 @@ resource "cloudflare_record" "mx" {
     "10" = "mail.protonmail.ch"
     "20" = "mailsec.protonmail.ch"
   }
-  zone_id    = var.cloudflare_zone_id
-  name       = var.domain
-  value      = each.value
-  type       = "MX"
-  priority   = each.key
+  zone_id  = var.cloudflare_zone_id
+  name     = var.domain
+  value    = each.value
+  type     = "MX"
+  priority = each.key
 }
 
 resource "cloudflare_record" "dkim" {
   for_each = {
-    "protonmail._domainkey" = "${var.dkim_1}"
+    "protonmail._domainkey"  = "${var.dkim_1}"
     "protonmail2._domainkey" = "${var.dkim_2}"
     "protonmail3._domainkey" = "${var.dkim_3}"
   }
-  zone_id    = var.cloudflare_zone_id
-  name       = "${each.key}.${var.domain}"
-  value      = each.value
-  type       = "CNAME"
+  zone_id = var.cloudflare_zone_id
+  name    = "${each.key}.${var.domain}"
+  value   = each.value
+  type    = "CNAME"
 }
 
 resource "cloudflare_record" "spf" {
-  zone_id    = var.cloudflare_zone_id
-  name       = var.domain
-  value      = var.spf_record
-  type       = "TXT"
+  zone_id = var.cloudflare_zone_id
+  name    = var.domain
+  value   = var.spf_record
+  type    = "TXT"
 }
 
 resource "cloudflare_record" "dmarc" {
-  zone_id    = var.cloudflare_zone_id
-  name       = "_dmarc.${var.domain}"
-  value      = "v=DMARC1; p=${var.dmarc_policy}"
-  type       = "TXT"
+  zone_id = var.cloudflare_zone_id
+  name    = "_dmarc.${var.domain}"
+  value   = "v=DMARC1; p=${var.dmarc_policy}"
+  type    = "TXT"
 }
